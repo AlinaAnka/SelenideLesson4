4568dad3e42ee6a738bec50f46b05cf7069b6857
Alina K <alina885@mail.ru>
2025-03-08 23:02:28 +0300

    fix

--- src/test/java/HwLesson4Test.java
+++ src/test/java/HwLesson4Test.java
@@ -22,7 +22,17 @@ public class HwLesson4Test {
         $("#wiki-pages-filter").setValue("SoftAssertions");
         $$("a.Truncate-text").shouldHave(itemWithText("SoftAssertions"));
         $$("a.Truncate-text").findBy(text("SoftAssertions")).click();
-        $$("span.pl-c1").filterBy(text("ExtendWith")).shouldHave(size(1));
+        $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
 
     }
 }
